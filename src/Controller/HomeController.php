5f3ee30c38bf8d2c07f3973330a5eeb7dd0d66d7
desiree <deutodesiree@gmail.com>
2019-12-01 23:14:29 +0100

    first Controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -16,6 +16,34 @@ class HomeController extends AbstractController
 	 */
 	public function home():Response
 	{
-		return new Response("toto");
+
+		$prenoms = ['Lior' => 12, 'Dez' => 27];
+
+		return $this->render(
+			"home.html.twig",
+			[
+				'title' => 'Welcome everybody!',
+				'age' => 12,
+				'prenoms' => $prenoms
+			]
+		);
+	}
+
+
+	/**
+	 * @Route("/hello/{prenom}/{age}", name="hello", methods={"GET"})
+	 * @Route("/hello", name="hello_base", methods={"GET"})
+	 * @Route("/hello/{prenom}", name="hello_prenom", methods={"GET"})
+	 *
+	 * @return Response
+	 */
+	public function hello($prenom = "default", $age = 0):Response
+	{
+			return $this->render(
+				"hello.html.twig",
+				[
+					"prenom" => $prenom,
+					"age" => $age
+				]);
 	}
 }
